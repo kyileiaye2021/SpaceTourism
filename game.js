ba197104fc745cafe39bf7741288622e0b68e04f
Eugene Teng <tengeugene18@gmail.com>
2023-10-07 21:27:47 -0700

    Modified game.js movement for the spaceship

--- game.js
+++ game.js
@@ -1,27 +1,33 @@
-
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-// Shapeship
+// Spaceship
 let spaceshipX = canvas.width / 2;
 let spaceshipY = canvas.height / 2;
 const spaceshipWidth = 30;
 const spaceshipHeight = 30;
 
-const planetSpeed = 2; // Adjust the speed as needed
+const planetSpeed = 2.5; // Adjust the speed as needed (faster)
+const acceleration = 1; // Adjust acceleration (higher)
+const friction = 0.98; // Friction factor to slow down the spaceship
+const stopThreshold = 0.5; // Threshold for stopping
+const maxVelocity = 5; // Maximum velocity
 
-//Planet
+// Planet
 let planetCircleX = canvas.width - 50;
-let planetCircleY = canvas.height/2;
+let planetCircleY = canvas.height / 2;
 const planetCircleRadius = 50;
 
 let planetCircleA = canvas.width - 200;
-let planetCircleB = canvas.height/3;
+let planetCircleB = canvas.height / 3;
 
-
-// Movement variables
+// Velocity variables for X and Y directions
 let dx = 0;
 let dy = 0;
+
+// Direction variables
+let lastDirectionX = 0;
+let lastDirectionY = 0;
 
 // Function to draw the circle
 function drawCircle(circX, circY, rad, color) {
@@ -32,7 +38,7 @@ function drawCircle(circX, circY, rad, color) {
     ctx.closePath();
 }
 
-  function drawSpaceship() {
+function drawSpaceship() {
     const spaceshipImg = new Image();
     spaceshipImg.src = "rocket-th.png";
     ctx.drawImage(spaceshipImg, spaceshipX, spaceshipY, spaceshipWidth, spaceshipHeight);
@@ -43,19 +49,37 @@ function updateCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Draw the circle
-    
     drawCircle(planetCircleA, planetCircleB, 70, "#ffcc00");
     drawSpaceship();
     checkCollision();
 
-    // Update the spaceship's position (you can still use dx and dy for movement)
+    // Update the spaceship's velocity based on acceleration for X and Y directions
+    if (Math.abs(dx) < maxVelocity) {
+        dx += lastDirectionX * acceleration;
+    }
+    if (Math.abs(dy) < maxVelocity) {
+        dy += lastDirectionY * acceleration;
+    }
+
+    // Apply friction to slow down the spaceship
+    dx *= friction;
+    dy *= friction;
+
+    // Apply momentum and stop the spaceship when below the threshold
+    if (Math.abs(dx) < stopThreshold) {
+        dx = 0;
+    }
+    if (Math.abs(dy) < stopThreshold) {
+        dy = 0;
+    }
+
+    // Update the spaceship's position
     spaceshipX += dx;
     spaceshipY += dy;
 
     // Keep the spaceship within the canvas boundaries
     if (spaceshipX < 0) {
         spaceshipX = 0;
-
     }
     if (spaceshipX + spaceshipWidth > canvas.width) {
         spaceshipX = canvas.width - spaceshipWidth;
@@ -66,63 +90,69 @@ function updateCanvas() {
     if (spaceshipY + spaceshipHeight > canvas.height) {
         spaceshipY = canvas.height - spaceshipHeight;
     }
-
-    
 }
 
 function checkCollision() {
     // Calculate the distance between the centers of the spaceship and the planet
-
-    const rightEdge = planetCircleA + planetCircleRadius;
-    const leftEdge = planetCircleA - planetCircleRadius;
-    const topEdge = planetCircleB + planetCircleRadius;
-    const bottomEdge = planetCircleB - planetCircleRadius;
-  
-    
-
+    const distance = Math.sqrt(
+        Math.pow(spaceshipX + spaceshipWidth / 2 - planetCircleA, 2) +
+        Math.pow(spaceshipY + spaceshipHeight / 2 - planetCircleB, 2)
+    );
 
     // Check if the distance is less than the sum of the spaceship's and planet's radii
-    if ((spaceshipX > leftEdge && spaceshipX < rightEdge) && (spaceshipY < topEdge && spaceshipY > bottomEdge)) {
+    if (distance < spaceshipWidth / 2 + planetCircleRadius) {
         // Collision detected! Navigate to a new page
         window.location.href = "planetwindow.html";
     }
 }
 
 // Function to handle keyboard input
+const keys = {}; // Object to track key states
+
 function handleKeyDown(event) {
-    if (event.key === "ArrowLeft") {
-        dx = -2;
-    } else if (event.key === "ArrowRight") {
-        dx = 2;
-    } else if (event.key === "ArrowUp") {
-        dy = -2;
-    } else if (event.key === "ArrowDown") {
-        dy = 2;
-    }
+    keys[event.key] = true;
+    calculateVelocity();
 }
 
 function handleKeyUp(event) {
-    if (
-        event.key === "ArrowLeft" ||
-        event.key === "ArrowRight" ||
-        event.key === "ArrowUp" ||
-        event.key === "ArrowDown"
-    ) {
-  //dx = 0;
-  //dy = 0;
+    keys[event.key] = false;
+    calculateVelocity();
+}
+
+function calculateVelocity() {
+    dx = 0;
+    dy = 0;
+
+    if (keys["ArrowLeft"]) {
+        lastDirectionX = -planetSpeed;
+    }
+    if (keys["ArrowRight"]) {
+        lastDirectionX = planetSpeed;
+    }
+    if (keys["ArrowUp"]) {
+        lastDirectionY = -planetSpeed;
+    }
+    if (keys["ArrowDown"]) {
+        lastDirectionY = planetSpeed;
     }
 }
 
-//Calculate distance between two circles
-var selectedPlanets = [];
+// Continuously apply last direction even after keys are released
+function applyLastDirection() {
+    if (!keys["ArrowLeft"] && !keys["ArrowRight"]) {
+        lastDirectionX = 0;
+    }
+    if (!keys["ArrowUp"] && !keys["ArrowDown"]) {
+        lastDirectionY = 0;
+    }
+}
+
 // Event listeners
 document.addEventListener("keydown", handleKeyDown);
 document.addEventListener("keyup", handleKeyUp);
-const form = document.querySelector('form');
-var arr = [];
-
-
-            
 
 // Game loop
-setInterval(updateCanvas, 10);
+setInterval(() => {
+    updateCanvas();
+    applyLastDirection();
+}, 10);
